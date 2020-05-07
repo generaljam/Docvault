d8109aee0ea2dd1e6d04298b6586d2e52a058648
Rajarajan <40290063+generaljam@users.noreply.github.com>
2020-05-07 19:49:50 +0530

    Update resnext_GPU.py

--- resnext_GPU.py
+++ resnext_GPU.py
@@ -134,7 +134,7 @@ with torch.no_grad():
         outputs = net(images)
         _, predicted = torch.max(outputs, 1)
         c = (predicted == labels).squeeze()
-        for i in range(num_classes):
+        for i in range(4):
             label = labels[i]
             class_correct[label] += c[i].item()
             class_total[label] += 1
